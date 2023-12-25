91d5c96e9f31fb55835e145b994b5251a285c94e
maksak_il <maksak_il@mail.ru>
2023-12-25 04:58:21 +0300

    Fix of images

---
+++ app/Console/Commands/SyncMedia.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class SyncMedia extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync-media';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $images = Media::all();
+
+        $bar = $this->output->createProgressBar(count($images));
+
+        $bar->start();
+
+        foreach ($images as $image) {
+            \Artisan::call('media-library:regenerate', [
+                '--ids'=>$image->id
+            ]);
+
+            $bar->advance();
+        }
+
+        $bar->finish();
+    }
+}
