9141dda87ccd5c1f4d9209cfbabc5309221db1e8
maksakoviliya <maksak_il@mail.ru>
2024-01-14 20:43:16 -0800

    Update ApartmentsChart.php

--- app/Filament/Resources/NoResource/Widgets/ApartmentsChart.php
+++ app/Filament/Resources/NoResource/Widgets/ApartmentsChart.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\NoResource\Widgets;
 
 use App\Models\Apartment;
+use App\Models\Reservation;
+use App\Models\ReservationRequest;
 use Carbon\Carbon;
+use Filament\Support\Colors\Color;
 use Filament\Widgets\ChartWidget;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
@@ -29,19 +32,55 @@ class ApartmentsChart extends ChartWidget
             ->where('created_at', '<=', now()->startOfMonth())
         ->count();
 
+        $reservation_requests_data = Trend::model(ReservationRequest::class)
+            ->between(
+                start: now()->startOfMonth(),
+                end: now(),
+            )
+            ->perDay()
+            ->count();
+        $color = Color::Blue[500];
+
+        $reservations_data = Trend::model(Reservation::class)
+            ->between(
+                start: now()->startOfMonth(),
+                end: now(),
+            )
+            ->perDay()
+            ->count();
+        $reservation_color = Color::Green[500];
+
         return [
             'datasets' => [
                 [
                     'label' => 'Жилье',
-                    
+
                     'data' => $data->map(function (TrendValue $value) use (&$total) {
                         $total += $value->aggregate;
 
                         return $total;
                     }),
                 ],
+                [
+                    'label' => 'Запросы на бронирование',
+
+                    'data' => $reservation_requests_data->map(function (TrendValue $value) {
+                        return $value->aggregate;
+                    }),
+                    'backgroundColor' => "rgb($color)",
+                    'borderColor' => "rgb($color)",
+                ],
+                [
+                    'label' => 'Бронирования',
+
+                    'data' => $reservations_data->map(function (TrendValue $value) {
+                        return $value->aggregate;
+                    }),
+                    'backgroundColor' => "rgb($reservation_color)",
+                    'borderColor' => "rgb($reservation_color)",
+                ],
             ],
-            'labels' => $data->map(fn (TrendValue $value) => Carbon::parse($value->date)->format('d.m.Y')),
+            'labels' => $reservation_requests_data->map(fn (TrendValue $value) => Carbon::parse($value->date)->format('d.m.Y')),
         ];
     }
 
