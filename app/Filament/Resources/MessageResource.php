7221695d4fa0e6679485b2e05ceaf0d2be0dd030
maksak_il <maksak_il@mail.ru>
2024-11-01 03:02:06 +0300

    Update MessageResource.php

--- app/Filament/Resources/MessageResource.php
+++ app/Filament/Resources/MessageResource.php
@@ -52,7 +52,7 @@ class MessageResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array
