ea2593392501fac0d6296dbdfe512b536477de8c
maksakoviliya <maksak_il@mail.ru>
2024-12-08 18:07:08 +0300

    Rebuild

---
+++ public/build/assets/ReservationCreatedNotification-ef622687.js
@@ -0,0 +1,1 @@
+import i from"./NotificationItem-5ce6f165.js";import{l as e,w as a,a as n,f as c}from"./app-f9def4a7.js";const _={__name:"ReservationCreatedNotification",props:{notification:{type:Object}},setup(t){const o=t;return(r,s)=>(n(),e(i,{notification:o.notification},{title:a(()=>[c(" Бронирование успешно создано ")]),_:1},8,["notification"]))}};export{_ as default};
