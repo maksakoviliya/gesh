b642bcee3b6113bf7aa107e2ded529fd1461dfd9
maksakoviliya <maksak_il@mail.ru>
2024-12-16 22:40:12 +0300

    debug

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -104,7 +104,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-//            ApartmentsUnderManagementRelationManager::class,
+            ApartmentsUnderManagementRelationManager::class,
         ];
     }
 
